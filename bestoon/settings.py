670b5add36ec72f0f904607e4c0c57a72972daa4
MilaD Allahverdi <milad@ubuntu.rampco.com>
2017-10-15 15:03:08 +0330

    change allowed_hosts and create static_root

--- bestoon/settings.py
+++ bestoon/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.11.0/24']
